580d2be09a400e5f6e2a4bb699f1d7e66b86a306
matu-lgs <442285570@qq.com>
2023-01-09 16:08:16 +0900

    anuglar add

--- tauri/demo01/src/app/app.component.ts
+++ tauri/demo01/src/app/app.component.ts
@@ -3,42 +3,8 @@ import { invoke } from "@tauri-apps/api/tauri";
 
 @Component({
   selector: "app-root",
-  template: `
-    <div class="container">
-      <h1>Welcome to Tauri + Angular!</h1>
-
-      <div class="row">
-        <a href="https://tauri.app" target="_blank">
-          <img src="/assets/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://angular.io" target="_blank">
-          <img
-            src="/assets/angular.svg"
-            class="logo angular"
-            alt="Angular logo"
-          />
-        </a>
-      </div>
-
-      <p>Click on the logos to learn more about the frameworks</p>
-
-      <div class="row">
-        <div>
-          <input #greetInput id="greet-input" placeholder="Enter a name..." />
-          <button type="button" (click)="greet(greetInput.value)">Greet</button>
-        </div>
-      </div>
-
-      <p>{{ greetingMessage }}</p>
-    </div>
-  `,
-  styles: [
-    `
-      .logo.angular:hover {
-        filter: drop-shadow(0 0 2em #e32727);
-      }
-    `,
-  ],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
   standalone: true,
 })
 export class AppComponent {
@@ -49,4 +15,16 @@ export class AppComponent {
       this.greetingMessage = text;
     });
   }
+}
+
+if (typeof Worker !== "undefined") {
+  // Create a new
+  const worker = new Worker(new URL("./app.worker", import.meta.url));
+  worker.onmessage = ({ data }) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage("hello");
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
 }
---
+++ tauri/demo01/src/app/pages/demo03/demo03.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Demo03Component } from './demo03.component';
+
+describe('Demo03Component', () => {
+  let component: Demo03Component;
+  let fixture: ComponentFixture<Demo03Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Demo03Component ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Demo03Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
