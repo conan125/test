f357760959d88b108bad5cb7ef27a762c79b5bbe
matu-lgs <442285570@qq.com>
2023-01-09 18:53:50 +0900

    angular module

--- tauri/demo01/src/app/app.component.ts
+++ tauri/demo01/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { invoke } from "@tauri-apps/api/tauri";
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-  standalone: true,
+  standalone: false,
 })
 export class AppComponent {
   greetingMessage = "";
--- tauri/demo01/src/main.ts
+++ tauri/demo01/src/main.ts
@@ -1,11 +1,15 @@
-import { enableProdMode } from "@angular/core";
-import { bootstrapApplication } from "@angular/platform-browser";
-import { AppComponent } from "./app/app.component";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { environment } from "./environments/environment";
+import { AppModule } from "./app/app.module";
 
-if (environment.production) {
-  enableProdMode();
+function bootstrap() {
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
 }
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+if (document.readyState === "complete") {
+  bootstrap();
+} else {
+  document.addEventListener("DOMContentLoaded", bootstrap);
+}
